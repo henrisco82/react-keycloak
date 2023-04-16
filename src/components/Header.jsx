819de1f5e1cb3bfcd94d60120542c253cb1701c2
Henry Odohi Unah <henry.unah@gmail.com>
2023-04-16 17:36:05 +0200

    added links to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,14 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { GlobalStyles } from '@mui/material';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+const rightLink = {
+    fontSize: 16,
+    color: 'common.white',
+    ml: 3,
+  };
 
 
 const Header = () => {
@@ -16,6 +24,25 @@ const Header = () => {
           <Typography variant="h6" color="inherit" noWrap>
             Secured App
           </Typography>
+          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+            <Link
+              color="inherit"
+              variant="h6"
+              underline="none"
+              href="#"
+              sx={rightLink}
+            >
+              {'Log In'}
+            </Link>
+            <Link
+              variant="h6"
+              underline="none"
+              href="#"
+              sx={{ ...rightLink }}
+            >
+              {'Log Out'}
+            </Link>
+          </Box>
         </Toolbar>
       </AppBar>
       </>
